aa321ce6c236d8fd45b10a9569e8fc2d8ee20afe
Mario Serano <marioserano55@gmail.com>
2022-07-05 16:41:27 +0700

    feat: added playground and remove postcss rollup module

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,15 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
+
+// MS: Currently, we don't need to use postcss. But, if in the future we want to use things like: tailwind, etc. we might need this.
 import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
 
-export default {
+const rollupConfig = {
   input: "src/index.ts",
   external: ["styled-components"],
-  globals: { "styled-components": "styled" },
   output: [
     {
       file: packageJson.main,
@@ -27,8 +28,7 @@ export default {
     resolve(),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
-    postcss({
-      extensions: [".css"],
-    }),
   ],
 };
+
+export default rollupConfig;
