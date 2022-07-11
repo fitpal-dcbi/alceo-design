24a4f41c5eeb91b1b03296789608290944df25fa
Mario Serano <marioserano55@gmail.com>
2022-07-11 15:42:41 +0700

    fix: add named exportType to support svg attributes

--- playground/index.tsx
+++ playground/index.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Modal } from "../lib";
+import { Icon } from "../lib";
 
 const App = () => {
   const [isOpen, setIsOpen] = React.useState<boolean>(false);
   return (
     <div>
-      <button onClick={() => setIsOpen(true)}>open!</button>
-      {isOpen && <Modal onClose={() => setIsOpen(false)}>yo!</Modal>}
+      <Icon />
     </div>
   );
 };
--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,13 +1,11 @@
 import React, { FC } from "react";
 import { ReactComponent as Arrow } from "../../assets/arrow.svg";
-import { StyledIcon } from "./style";
 
 // MS: To be deleted later, shows the use case of svgr
 const Icon: FC = (props) => {
   return (
     <div>
-      <Arrow />
-      <StyledIcon />
+      <Arrow className="tesClassName" />
     </div>
   );
 };
