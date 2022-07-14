487dd2743ce0706ee3d3a143bf688cc7305e7aba
wahib-sirka <wahib@sirka.io>
2022-07-14 08:29:50 +0700

    feat: add documentation in iconOnly props

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -3,6 +3,7 @@ import { Meta, Story } from "@storybook/react";
 
 import { Badge as BadgeComponent } from "./index";
 import { BaseBadgeProps } from "./type";
+import { ReactComponent as ExampleIcon } from "../../assets/icon-lock-active.svg";
 
 export default {
   title: "Components/Badge",
@@ -38,13 +39,21 @@ export const Badge: Story<BaseBadgeProps> = ({
   children = "label",
 }) => {
   return (
-    <BadgeComponent
-      variant={variant}
-      inverted={inverted}
-      hasInfoIcon={hasInfoIcon}
-      prefix={prefix}
-      iconOnly={iconOnly}
-      children={children}
-    />
+    <div>
+      <BadgeComponent
+        variant={variant}
+        inverted={inverted}
+        hasInfoIcon={hasInfoIcon}
+        prefix={prefix}
+        iconOnly={iconOnly}
+        children={iconOnly === true ? <ExampleIcon /> : children}
+      />
+      {iconOnly && (
+        <p style={{ fontSize: "12px", fontWeight: "400" }}>
+          icon just an example, <br />
+          we can customize this icon on parent component
+        </p>
+      )}
+    </div>
   );
 };
