78390cec089731d72e15eec7fa944024dbcd1cf9
AnisaMelinda <anisa@sirka.io>
2022-07-14 17:56:25 +0700

    fix: fixing checkbox index

--- src/components/Forms/Radio/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./Radio";
-export { default as Radio } from "./Radio";
-
-export * from "./ControlledRadio";
-export { default as ControlledRadio } from "./ControlledRadio";
-
-export * from "./style";
-export * from "./type";
--- src/components/Forms/index.ts
+++ src/components/Forms/index.ts
@@ -1,2 +1,2 @@
 export * from "./Switch";
-export * from "./Radio";
+export * from "./Checkbox";
