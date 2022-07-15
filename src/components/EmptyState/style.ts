2d14fdac48a2d643689af1a17de9a5333a5f4484
wahib-sirka <wahib@sirka.io>
2022-07-15 09:17:43 +0700

    feat: finished implementation of empty state

---
+++ src/components/EmptyState/style.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const StyledEmptyState = styled.div`
+  .empty-state__container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem 1.875rem;
+    justify-content: flex-start;
+
+    max-width: 23.5rem;
+    height: 100%;
+  }
+
+  .empty-state__title {
+    margin: 0;
+    font-weight: 600;
+    font-size: 1.5rem;
+    line-height: 2.375rem;
+    text-align: center;
+    color: #262626;
+    margin-bottom: 0.5rem;
+  }
+
+  .empty-state__description {
+    margin: 0;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 1.75rem;
+    color: #555555;
+    text-align: center;
+    margin-bottom: 1rem;
+  }
+
+  .empty-state__button-wrapper {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
---
+++ src/components/EmptyState/type.ts
@@ -0,0 +1,8 @@
+interface BaseEmptyStateProps {
+  image?: React.ReactNode;
+  title?: string;
+  desc?: string;
+  button?: React.ReactNode;
+}
+
+export type EmptyStateProps = BaseEmptyStateProps;
