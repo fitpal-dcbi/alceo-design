df889f024756ad34cb1112a957dfade202710500
AnisaMelinda <anisa@sirka.io>
2022-07-17 16:44:35 +0700

    fix: fixing style override

--- src/components/Forms/Checkbox/style.ts
+++ src/components/Forms/Checkbox/style.ts
@@ -4,44 +4,46 @@ import { BaseCheckboxProps } from "./index";
 export const StyledCheckboxContainer = styled.div<
 Pick<BaseCheckboxProps, "disabled" | "hasError">>`
   cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+  color: #000000;
 
-  .gjqXHB,
-  .czPEzn:active .Checkbox__IconContainer-sc-1xqef2c-0,
-  .bBxUxW, .iZGxCF, .edWdNF {
+  span {
+    font-weight: 400 !important;
+  };
+
+  label {
+    width: fit-content;
+    opacity: unset;
+  };
+
+  label div:first-of-type {
     border-radius: .8125rem;
     border: 1px solid;
-    border-color: #BAC7D5;
-  }
+    border-color: ${(props) => props.disabled ? '#E6E6E6' : '#BAC7D5'}; 
+    background-color: ${(props) => props.disabled ? '#E6E6E6' : 'unset'};
+  };
 
-  .RyIjk:hover .Checkbox__IconContainer-sc-1xqef2c-0,
-  .iJkZfb:hover .Checkbox__IconContainer-sc-1xqef2c-0,
-  .RyIjk:active .Checkbox__IconContainer-sc-1xqef2c-0,
-  .iJkZfb:active .Checkbox__IconContainer-sc-1xqef2c-0
-  {
-    border-color: ${(props) => props.disabled ? 'none' : '#5CA898'};
+  label:hover div:first-of-type,
+  label:active div:first-of-type
+   {
+    border-color: ${(props) => props.disabled ? '#E6E6E6' : 'unset'};
     box-shadow: ${(props) => props.disabled ? 'none' : '0px 0px 0px 2px rgba(220, 241, 237, 1)'};
-  }
+  };
 
-  .OvNsw .Checkbox__IconContainer-sc-1xqef2c-0 {
+  label > input[data-state="error"] + div {
     border-color: #EE3633 !important;
     box-shadow: 0px 0px 0px 2px rgba(255, 227, 226, 1) !important;
-  }
+  };
 
-  .edWdNF, 
-  .edWdNF:hover {
-    background: #E6E6E6;
-    border: 1px solid #DCDCDC;
-  }
+  label > input:checked + div {
+    background-color: ${(props) => props.disabled ? '#E6E6E6' : '#5CA898'};
+    border-color: ${(props) => props.disabled ? '#E6E6E6' : '#5CA898'};
+  };
 
-  .cpCxwI .Checkbox__IconContainer-sc-1xqef2c-0,
-  .bBxUxW {
-    background-color: #5CA898;
-    border-color: #5CA898;
-  }
-
-  .fAlffV:checked ~ .Checkbox__TextContainer-sc-1xqef2c-1 > .Checkbox__LabelText-sc-1xqef2c-3 {
-    font-weight: 400;
-  }
+  input:focus + div {
+    border: 1px solid;
+    border-color: ${(props) => props.disabled ? 'none' : '#5CA898'} !important;
+    box-shadow: none !important;
+  };
 `;
 
 export const StyledCheckboxWrapper = styled.div`
