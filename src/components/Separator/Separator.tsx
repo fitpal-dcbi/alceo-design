2e97debf31cf91cf0d9e3c81d0ff19c6f748c560
Mario Serano <marioserano55@gmail.com>
2022-07-19 21:37:27 +0700

    chore: added forwardref function before the functional component

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -1,11 +1,19 @@
-import React, { FC } from "react";
+import React from "react";
 
 import { StyledSeparator } from "./style";
 import { SeparatorProps } from "./type";
 
-const Separator: FC<SeparatorProps> = (props) => {
-  const { alignment = "left", indentation } = props;
-  return <StyledSeparator alignment={alignment} indentation={indentation} />;
-};
+const Separator = React.forwardRef<HTMLDivElement, SeparatorProps>(
+  (props, ref) => {
+    const { alignment = "left", indentation } = props;
+    return (
+      <StyledSeparator
+        ref={ref}
+        alignment={alignment}
+        indentation={indentation}
+      />
+    );
+  }
+);
 
 export default Separator;
